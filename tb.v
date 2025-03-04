6c4280bf200b99f8d75f5cd0b94bb826cae65c4b
swasthikm <59522858+swasthikm@users.noreply.github.com>
2025-03-04 12:53:11 -0500

    Add files via upload

---
+++ tb.v
@@ -0,0 +1,23 @@
+module tb;
+ 
+ 
+integer i = 0;
+ 
+reg clk = 0,sys_rst = 0;
+reg [15:0] din = 0;
+wire [15:0] dout;
+ 
+ 
+top dut(clk, sys_rst, din, dout);
+ 
+always #5 clk = ~clk;
+ 
+initial begin
+sys_rst = 1'b1;
+repeat(5) @(posedge clk);
+sys_rst = 1'b0;
+#800;
+$stop;
+end
+ 
+endmodule
